cafdeb49df684c4ddd26e7209955e8704c0972d1
Tobias Schutter <tobias@codepress.nl>
2023-04-12 14:02:46 +0200

    Added Site Filter

--- classes/ListScreenRepository/Filter/Site.php
+++ classes/ListScreenRepository/Filter/Site.php
@@ -5,22 +5,17 @@ namespace AC\ListScreenRepository\Filter;
 use AC\ListScreenCollection;
 use AC\ListScreenRepository\Filter;
 
-class Network implements Filter {
+class Site implements Filter {
 
-	public const KEYS = [
-		'wp-ms_sites',
-		'wp-ms_users',
-	];
-
-	protected function is_network( string $list_key ): bool {
-		return in_array( $list_key, self::KEYS, true );
+	protected function is_site( string $list_key ): bool {
+		return ! in_array( $list_key, Network::KEYS, true );
 	}
 
 	public function filter( ListScreenCollection $list_screens ): ListScreenCollection {
 		$collection = new ListScreenCollection();
 
 		foreach ( $list_screens as $list_screen ) {
-			if ( $this->is_network( $list_screen->get_key() ) ) {
+			if ( $this->is_site( $list_screen->get_key() ) ) {
 				$collection->add( $list_screen );
 			}
 		}
